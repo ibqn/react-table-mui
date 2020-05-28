0bd88fdd6ef1425aba950e0887cbbdd477d22388
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-05-28 23:42:48 +0200

    add basic dialog

--- src/components/add-user-dialog.jsx
+++ src/components/add-user-dialog.jsx
@@ -4,14 +4,27 @@ import Tooltip from '@material-ui/core/Tooltip'
 import IconButton from '@material-ui/core/IconButton'
 import AddIcon from '@material-ui/icons/Add'
 
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
 const AddUserDialog = (props) => {
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => setOpen(true)
+
+  const handleClose = () => setOpen(false)
+
   return (
     <div>
       <Tooltip title="Add">
-        <IconButton aria-label="Add">
+        <IconButton aria-label="Add" onClick={handleClickOpen}>
           <AddIcon />
         </IconButton>
       </Tooltip>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add User</DialogTitle>
+      </Dialog>
     </div>
   )
 }
