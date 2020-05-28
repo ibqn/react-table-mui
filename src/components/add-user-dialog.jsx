e175aee074ecaee41fbbb0f6e5fa4701cb397bbc
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-05-29 00:44:45 +0200

    add more content to dialog

--- src/components/add-user-dialog.jsx
+++ src/components/add-user-dialog.jsx
@@ -6,13 +6,32 @@ import AddIcon from '@material-ui/icons/Add'
 
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
+
+import Button from '@material-ui/core/Button'
+import Switch from '@material-ui/core/Switch'
+import TextFeld from '@material-ui/core/TextField'
+
+const initialUser = {
+  firstName: '',
+}
 
 const AddUserDialog = (props) => {
+  const [user, setUser] = useState(initialUser)
   const [open, setOpen] = useState(false)
 
   const handleClickOpen = () => setOpen(true)
 
   const handleClose = () => setOpen(false)
+
+  const hangeChange = (name) => ({ target: { value } }) => {
+    // console.log('on change', name, 'value is', value)
+    setUser({ ...user, [name]: value })
+  }
+
+  const handleAdd = () => {}
 
   return (
     <div>
@@ -24,6 +43,29 @@ const AddUserDialog = (props) => {
 
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Add User</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Demo add item to react table.</DialogContentText>
+          <TextFeld
+            autoFocus
+            margin="dense"
+            label="First Name"
+            type="text"
+            fullWidth
+            value={user.firstName}
+            onChange={hangeChange('firstName')}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Tooltip title="Add multiple users">
+            <Switch />
+          </Tooltip>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleAdd} color="primary">
+            Add
+          </Button>
+        </DialogActions>
       </Dialog>
     </div>
   )
