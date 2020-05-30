2ff2974e226c606cefe7f523d77334d786eb61f7
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-05-30 22:29:50 +0200

    disable strict mode to avoid warning caused by material ui

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './app'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 )
 
